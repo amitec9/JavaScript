435c39676adba73611bedd34c3a0f5644439f0cf
Amit Kumar <eramit3096@gmail.com>
2026-01-21 10:15:12 +0530

    count

---
+++ Top_50 _Array _& _String_Interview_Questions/Count_odd.js
@@ -0,0 +1,15 @@
+
+// Count odd Numbers
+function countOdd(arr){
+   let count =0;
+   for(let i=0;i<arr.length;i++){
+       if(arr[i] % 2 !== 0){
+           count++;
+       }
+   }
+  
+   return count;
+}
+console.log( countOdd([
+ 1,2,3,6,0,8,5
+]))
